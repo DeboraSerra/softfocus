d045e70fb9d1539e91d7f2aaa5497f50d3fff18d
debora <debora.r.serra@gmail.com>
2022-09-02 15:20:07 -0300

    feat: adiciona barra de busca por cps

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,52 @@
+import React, { useContext, useState } from 'react';
+import { Context } from '../context/Provider';
+import { SButton, SFormHorizontal, SInput } from '../styles';
+import style from '../styles/SearchBar.module.css';
+
+const SearchBar = () => {
+  const [state, setState] = useState({
+    cpf: '',
+  });
+  const [error, setError] = useState('');
+  const { getCommunications } = useContext(Context);
+
+  const handleChange = ({ target }) => {
+    let { value } = target;
+    if (value.includes('.')) value = value.replace('.', '')
+    if (value.includes('-')) value = value.replace('-', '');
+    setState({
+      cpf: value,
+    })
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isNaN(state.cpf)) {
+      setError('CPF precisa conter apenas números')
+      return;
+    }
+    if (state.cpf.length !== 11) {
+      setError('CPF precisa conter exatamente 11 números')
+      return;
+    }
+    setError('');
+    getCommunications(state.cpf);
+  }
+
+  return (
+    <form className={ style.form } onSubmit={ handleSubmit }>
+      <label htmlFor="search">Pesquisar registro por CPF:
+        <input
+          className={ style.input }
+          type="text"
+          id="search"
+          value={ state.search }
+          onChange={ handleChange }
+        />
+      </label>
+      <button className={ style.button } type="submit">Pesquisar</button>
+      {error && <p>{error}</p>}
+    </form>
+  )
+}
+
+export default SearchBar;
